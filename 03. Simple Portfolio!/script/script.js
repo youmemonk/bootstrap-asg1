2cfbfdbc6047619a06f93b821a2471b83837059c
youmemonk <ukeym160@gmail.com>
2021-08-06 01:25:43 +0530

    Menu Bar for Mobile - Completed : CSS & JS

---
+++ 03. Simple Portfolio!/script/script.js
@@ -0,0 +1,28 @@
+// SHOW MENU : HIDDEN
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+// SHOW MENU : VISIBLE
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// MENU HIDDEN
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+// Remove Mobile Menu
+const navLink = document.querySelectorAll(".nav_link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // on clicking, the show menu class is removed
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
