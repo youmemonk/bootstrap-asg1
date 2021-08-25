ac983985345fcf70c9cff6e20e3bf0329637fbeb
youmemonk <ukeym160@gmail.com>
2021-08-26 00:27:52 +0530

    react based simple todo

---
+++ 05. React/simple-todo/src/todosData.js
@@ -0,0 +1,33 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Finish Laptop touchup.",
+    completed: true,
+  },
+
+  {
+    id: 2,
+    text: 'Complete the Styling',
+    completed: false
+  },
+
+  {
+    id: 3,
+    text: 'Mowing the Lawn',
+    completed: false
+  },
+
+  {
+    id: 4,
+    text: 'Finishing Kafka on the shore',
+    completed: true
+  },
+
+  {
+    id: 5,
+    text: 'Finish Norwegian wood',
+    completed: false
+  }
+];
+
+export default todosData;
