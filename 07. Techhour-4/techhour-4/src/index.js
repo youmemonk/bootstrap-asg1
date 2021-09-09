2de4a7a8992ebba82405ffdaaf6d50d79cacc5c1
youmemonk <ukeym160@gmail.com>
2021-09-09 13:28:25 +0530

    new-app init

--- 07. Techhour-4/techhour-4/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App";
-
-ReactDOM.render(<App 
-    // headerProp="header from props"
-    // contentProp = "content from props"
-/>, document.getElementById("root"));
