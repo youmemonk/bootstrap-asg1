24e03ee93fe2b0d3800f7612af660f97c5eed717
youmemonk <ukeym160@gmail.com>
2021-09-09 14:05:28 +0530

    navbar Added

---
+++ 05. React/sign-ui/src/components/signupcomponents.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SignUp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>React SignUp Component.</h3>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
