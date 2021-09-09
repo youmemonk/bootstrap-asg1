003d6b1945630987a965e4e0ccddb2183eb29735
youmemonk <ukeym160@gmail.com>
2021-09-09 14:54:07 +0530

    Nav Added.

--- 05. React/signup-ui/src/App.js
+++ 05. React/signup-ui/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/logincomponent";
+import SignUp from "./components/signupcomponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/sign-in"}>
+              youmemonk
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarToggleDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-in"}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-up"}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route path="/sign-in" component={Login} />
+              <Route path="/sign-up" component={SignUp} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ 05. React/signup-ui/src/components/logincomponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>React Login Component</h3>
+      </div>
+    );
+  }
+}
